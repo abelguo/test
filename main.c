3cf7292e8504aa7726d26fbf9e23e2723350a9b2
abelguo <abelguoyong@gmail.com>
2015-01-10 23:14:55 +0800

    test

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /*************************************************************************
-    > File Name: main.c
-    > Author: guoyong
-    > Mail: abelguoyong@qq.com 
-    > Created Time: Sat 21 Jun 2014 23:17:26 CST
+  > File Name: main.c
+  > Author: guoyong
+  > Mail: abelguoyong@qq.com 
+  > Created Time: Sat 21 Jun 2014 23:17:26 CST
  ************************************************************************/
 #include"readfile.h"
 #include<stdio.h>
@@ -11,6 +11,12 @@ int main(int argc,char *argv[])
 {
 	int a;
 	char *file;
+	if(argc<2)
+	{
+		printf("you must input file name\n");
+		return -1;
+
+	}
 	file=(char *)My_Map(argv[1]);
 	printf("the number is %s\n",file);
 	if((a=Get_Int(argv[2],argv[3],file))==-1)
@@ -18,6 +24,15 @@ int main(int argc,char *argv[])
 		printf("have error\n");
 		return -1;
 	}
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
+	printf("the number is %d\n",a);
 	printf("the number is %d\n",a);
 	printf("the number is %d\n",a);
 	my_umap(file);
